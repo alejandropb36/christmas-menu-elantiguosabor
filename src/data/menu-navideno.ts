2810e49149fefe7bb0b3a78cee84c51a05218788
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-17 23:14:51 -0600

    feat: menu items improvements

--- src/data/menu-navideno.ts
+++ src/data/menu-navideno.ts
@@ -15,36 +15,108 @@ export const menuData = {
                     description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
                     price: '$3,100 p/pieza',
                     image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Pechuga de Pavo',
+                    description: 'Pechuga de Pavo sin hueso rellena, con gravy.',
+                    price: '$1,500 p/kilo',
+                    image: '/img/pavo.jpg'
                 }
+            ]
+        },
+        {
+            title: 'Carnes',
+            content: [
+                {
+                    title: 'Pierna',
+                    description: `<ul> <li>Mechadas</li> <li>Al vino</li> <li>A la ciruela</li>
+                    <li>Al chipotle</li> <li>Campiñones</li> </ul>`,
+                    price: '$580 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Lomo',
+                    description: `<ul> <li>Relleno</li> <li>Mechado</li> <li>A la ciruela</li>
+                    <li>Al chipotle</li> <li>Al vino</li> </ul>`,
+                    price: '$600 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Filete Miñon',
+                    description: '',
+                    price: '$900 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Filete al vino',
+                    description: '',
+                    price: '$900 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Filete wellington',
+                    description: '',
+                    price: '$1,200 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                
+            ]
+        },
+        {
+            title: 'Pescado',
+            content: [
+                {
+                    title: 'Bacalao',
+                    description: 'A la vizcaína',
+                    price: '$850 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
             ]
         },
         {
             title: 'Pastas',
             content: [
                 {
-                    title: 'Pavo Ahumado',
-                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
-                    price: '$3,100 p/pieza',
+                    title: 'Lasagna',
+                    description: 'Lasagna para 12 personas.',
+                    price: '$1,400 p/pieza',
                     image: '/img/pavo.jpg'
                 },
                 {
-                    title: 'Pavo Natural',
-                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
-                    price: '$3,000 p/pieza',
+                    title: 'Con camarones',
+                    description: 'Pasta con camarones.',
+                    price: '$350 p/kilo',
                     image: '/img/pavo.jpg'
                 },
                 {
-                    title: 'Pavo Ahumado',
-                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
-                    price: '$3,100 p/kilo',
+                    title: 'Jamón serrano',
+                    description: 'Pasta con jamón serrano.',
+                    price: '$350 p/kilo',
                     image: '/img/pavo.jpg'
                 },
                 {
-                    title: 'Pavo Ahumado',
-                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
-                    price: '$3,100 p/kilo',
+                    title: 'Alfredo',
+                    description: 'Pasta Alfredoo.',
+                    price: '$220 p/kilo',
                     image: '/img/pavo.jpg'
-                }
+                },
+                {
+                    title: 'Al gusto',
+                    description: '',
+                    price: 'Preguntar por precio',
+                    image: '/img/pavo.jpg'
+                },
+            ]
+        },
+        {
+            title: 'Cremas',
+            content: [
+                {
+                    title: 'Champiñones',
+                    description: 'Crema de campiñones',
+                    price: '$200 p/litro',
+                    image: '/img/pavo.jpg'
+                },
             ]
         },
         
