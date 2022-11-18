968dd6ae27e538711a52e9ba268a8f39000418f1
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-17 22:31:28 -0600

    feat: menu navideno first version

---
+++ src/data/menu-navideno.ts
@@ -0,0 +1,52 @@
+export const menuData = {
+    year: '2022',
+    sections: [
+        {
+            title: 'Pavos',
+            content: [
+                {
+                    title: 'Pavo Natural',
+                    description: 'Pavo natural glaseado con mermelada de chabacano o arandano',
+                    price: '$3,000 p/pieza',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Pavo Ahumado',
+                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
+                    price: '$3,100 p/pieza',
+                    image: '/img/pavo.jpg'
+                }
+            ]
+        },
+        {
+            title: 'Pastas',
+            content: [
+                {
+                    title: 'Pavo Ahumado',
+                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
+                    price: '$3,100 p/pieza',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Pavo Natural',
+                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
+                    price: '$3,000 p/pieza',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Pavo Ahumado',
+                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
+                    price: '$3,100 p/kilo',
+                    image: '/img/pavo.jpg'
+                },
+                {
+                    title: 'Pavo Ahumado',
+                    description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
+                    price: '$3,100 p/kilo',
+                    image: '/img/pavo.jpg'
+                }
+            ]
+        },
+        
+    ]
+}
