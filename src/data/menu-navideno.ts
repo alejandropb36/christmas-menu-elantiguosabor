631db26d6bdbdb59461b052b9600e549913c42db
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-24 20:01:17 -0600

    Add menu images

--- src/data/menu-navideno.ts
+++ src/data/menu-navideno.ts
@@ -8,19 +8,19 @@ export const menuData = {
                     title: 'Pavo Natural',
                     description: 'Pavo natural glaseado con mermelada de chabacano o arandano',
                     price: '$3,000 p/pieza',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pavos/pavo.jpg'
                 },
                 {
                     title: 'Pavo Ahumado',
                     description: 'Pavo ahumado glaseado con mermelada de chabacano o arandano',
                     price: '$3,100 p/pieza',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pavos/pavo.jpg'
                 },
                 {
                     title: 'Pechuga de Pavo',
                     description: 'Pechuga de Pavo sin hueso rellena, con gravy.',
                     price: '$1,500 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pavos/pechuga-rellena.jpg'
                 }
             ]
         },
@@ -32,34 +32,57 @@ export const menuData = {
                     description: `<ul> <li>Mechadas</li> <li>Al vino</li> <li>A la ciruela</li>
                     <li>Al chipotle</li> <li>Campiñones</li> </ul>`,
                     price: '$580 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/carnes/pierna-navidena.jpg'
                 },
                 {
                     title: 'Lomo',
                     description: `<ul> <li>Relleno</li> <li>Mechado</li> <li>A la ciruela</li>
                     <li>Al chipotle</li> <li>Al vino</li> </ul>`,
                     price: '$600 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/carnes/lomo-navideno.jpg'
                 },
                 {
                     title: 'Filete Miñon',
                     description: '',
                     price: '$900 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/carnes/filete-mignon.jpg'
                 },
                 {
                     title: 'Filete al vino',
                     description: '',
                     price: '$900 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/carnes/filete-vino.jpg'
                 },
                 {
                     title: 'Filete wellington',
                     description: '',
                     price: '$1,200 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/carnes/filete-wellington.jpg'
                 },
                 
+            ]
+        },
+        {
+            title: 'Ensaldas',
+            content: [
+                {
+                    title: 'Frutas',
+                    description: '(Especialidad de la casa)',
+                    price: '$280 p/litro',
+                    image: '/img/ensaladas/ensalada-frutas.jpg'
+                },
+                {
+                    title: 'Manzana',
+                    description: '',
+                    price: '$280 p/litro',
+                    image: '/img/ensaladas/ensalada-manzana.jpg'
+                },
+                {
+                    title: 'Betabel con nranja y nuez',
+                    description: '',
+                    price: '$280 p/litro',
+                    image: '/img/ensaladas/ensalada-betabel.jpg'
+                },
             ]
         },
         {
@@ -69,7 +92,7 @@ export const menuData = {
                     title: 'Bacalao',
                     description: 'A la vizcaína',
                     price: '$850 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pescado/bacalao-vizcaina.jpg'
                 },
             ]
         },
@@ -80,31 +103,31 @@ export const menuData = {
                     title: 'Lasagna',
                     description: 'Lasagna para 12 personas.',
                     price: '$1,400 p/pieza',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pastas/lasagna.jpg'
                 },
                 {
                     title: 'Con camarones',
                     description: 'Pasta con camarones.',
                     price: '$350 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pastas/pasta-camarones.png'
                 },
                 {
                     title: 'Jamón serrano',
                     description: 'Pasta con jamón serrano.',
                     price: '$350 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pastas/pasta-jamon-serrano.jpg'
                 },
                 {
                     title: 'Alfredo',
                     description: 'Pasta Alfredoo.',
                     price: '$220 p/kilo',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pastas/alfredo.jpg'
                 },
                 {
                     title: 'Al gusto',
                     description: '',
                     price: 'Preguntar por precio',
-                    image: '/img/pavo.jpg'
+                    image: '/img/pastas/pastas.jpg'
                 },
             ]
         },
@@ -115,7 +138,7 @@ export const menuData = {
                     title: 'Champiñones',
                     description: 'Crema de campiñones',
                     price: '$200 p/litro',
-                    image: '/img/pavo.jpg'
+                    image: '/img/cremas/crema-champinones.jpg'
                 },
             ]
         },
