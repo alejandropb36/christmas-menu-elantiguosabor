4f8795e5d8c61626c9df327d97778296bd60145a
Alejandro Ponce <alexponce36@hotmail.com>
2022-11-28 19:50:48 -0600

    update sit domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alejandropb36.github.io/",
-  base: "/christmas-menu-elantiguosabor",
+  site: "https://navidad.elantiguosabor.com",
 });
