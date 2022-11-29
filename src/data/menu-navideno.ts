8a0015d950148b87e905288d95429dfe9432cf79
Alejandro <alexponce36@hotmail.com>
2022-11-29 13:19:08 -0600

    fix: ensaladas name

--- src/data/menu-navideno.ts
+++ src/data/menu-navideno.ts
@@ -65,7 +65,7 @@ export const menuData = {
             ]
         },
         {
-            title: 'Ensaldas',
+            title: 'Ensaladas',
             content: [
                 {
                     title: 'Frutas',
